267cb91b9305dd72d16e7afa71afbd9319b48cef
Khayal91 <g_khayal@list.ru>
2023-04-24 16:49:21 +0400

    first project

---
+++ application/controllers/User_controller.php
@@ -0,0 +1,11 @@
+<?php
+
+class User_controller extends CI_Controller{
+    public function index (){
+        echo "Men index metoduyam";
+    }
+    public function about (){
+        echo "Men about metoduyam";
+    }
+}
+    
